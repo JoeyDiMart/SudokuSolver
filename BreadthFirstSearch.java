cb43577049a508c4ded75e3fd1fa5684b83450a4
JoeyDiMart <joseph.dimartino@spartans.ut.edu>
2024-11-25 13:06:42 -0500

    Short heading

--- BreadthFirstSearch.java
+++ BreadthFirstSearch.java
@@ -1,3 +1,7 @@
+/*
+Names: Joseph DiMartino, Nicole Scott, Daniel Jaffe
+Program: BredthFirstSearch: This class will contain methods for doing a BFS
+ */
 package SudokuSolver;
 
 public class BreadthFirstSearch {
--- DepthLimitedSearch.java
+++ DepthLimitedSearch.java
@@ -1,3 +1,7 @@
+/*
+Names: Joseph DiMartino, Nicole Scott, Daniel Jaffe
+Program: DepthLimitedSearch: This class will contain methods for doing a DLS.
+ */
 package SudokuSolver;
 
 public class DepthLimitedSearch {
